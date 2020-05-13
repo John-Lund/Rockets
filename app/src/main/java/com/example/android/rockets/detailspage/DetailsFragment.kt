51af19f6596ddad543089fd88582188b4a169c64
John Lund <john.lund@degree53.com>
2020-05-13 12:11:51 +0100

    Cleaned

--- app/src/main/java/com/example/android/rockets/detailspage/DetailsFragment.kt
+++ app/src/main/java/com/example/android/rockets/detailspage/DetailsFragment.kt
@@ -10,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.android.rockets.databinding.FragmentDetailsBinding
 
 
--- app/src/main/java/com/example/android/rockets/resultspage/ResultsFragment.kt
+++ app/src/main/java/com/example/android/rockets/resultspage/ResultsFragment.kt
@@ -5,7 +5,6 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.android.rockets.databinding.FragmentResultsBinding
 
